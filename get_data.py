ab0a32b4993cae6018e6ac49f0506faad9a303a4
Gary (MtGPU:container:ypeng_tf) <yygarypeng@gapp.nthu.edu.tw>
2023-10-30 02:22:13 +0000

    The first push.

---
+++ get_data.py
@@ -0,0 +1,136 @@
+import gc
+import glob
+import numpy as np
+import pandas as pd
+
+
+class DataProcessor:
+    def __init__(self):
+        self.GEV = 1e3
+        self.RMV_EVT = []
+
+    def load_files(self, path):
+        files_name = self.get_files_names(path)
+        self.files_name = files_name
+        self.files_name.sort()
+        self.files = [self.get_data(f) for f in self.files_name]
+        print(files_name)
+
+    def get_files_names(self, path):
+        files_name = glob.glob(path)
+        return files_name
+
+    def get_data(self, path):
+        try:
+            with np.load(path, allow_pickle=True) as f:
+                data_dict = {name: f[name] for name in f.files}
+                return pd.DataFrame(data_dict)
+        except FileNotFoundError:
+            print("File not found!")
+            return pd.DataFrame()
+
+    def process_lep(self, LepP, LepM):
+        lep_kin = (
+            pd.DataFrame(
+                {
+                    "lep_p_E": LepP["E"],
+                    "lep_p_px": LepP["px"],
+                    "lep_p_py": LepP["py"],
+                    "lep_p_pz": LepP["pz"],
+                    "lep_m_E": LepM["E"],
+                    "lep_m_px": LepM["px"],
+                    "lep_m_py": LepM["py"],
+                    "lep_m_pz": LepM["pz"],
+                }
+            )
+            / self.GEV
+        )
+        lep_kin.drop(self.RMV_EVT, inplace=True)
+        return lep_kin
+
+    def process_MET(self, MET):
+        nu_kin = (
+            pd.DataFrame(
+                {
+                    "di_nu_E": np.sqrt(
+                        np.square(34141 * np.ones(len(MET)))
+                        + np.square(MET["px"])
+                        + np.square(MET["py"])
+                    ),
+                    "di_nu_px": MET["px"],
+                    "di_nu_py": MET["py"],
+                    "di_nu_pz": np.zeros(len(MET)),
+                }
+            )
+            / self.GEV
+        )
+        nu_kin.drop(self.RMV_EVT, inplace=True)
+        return nu_kin
+
+    def process_dinu(self, NuP, NuM):
+        # Kinemetic info of neutirnos.
+        nu_kin = (
+            pd.DataFrame(
+                {
+                    "nu_E": NuP["E"] + NuM["E"],
+                    "nu_px": NuP["px"] + NuM["px"],
+                    "nu_py": NuP["py"] + NuM["py"],
+                    "nu_pz": NuP["pz"] + NuM["pz"],
+                }
+            )
+            / self.GEV
+        )
+        nu_kin.drop(self.RMV_EVT, inplace=True)
+        return nu_kin
+
+    def process_CGLMP(self, CGLMP):
+        CGLMP = pd.DataFrame(
+            {
+                "Bxy": CGLMP["Bxy"],
+                "Byz": CGLMP["Byz"],
+                "Bzx": CGLMP["Bzx"],
+            }
+        )
+        CGLMP.drop(self.RMV_EVT, inplace=True)
+        return CGLMP
+
+
+if __name__ == "__main__":
+    processor = DataProcessor()
+    path = "/root/work/truth/signal/*npz"
+    processor.load_files(path)
+
+    (
+        CGLMP,
+        Higgs,
+        LeadLep,
+        LepM,
+        LepP,
+        NuM,
+        NuP,
+        MET,
+        Wm,
+        Wp,
+        diLep,
+        SubLep,
+        Xi,
+    ) = processor.files
+
+    lep_kin = processor.process_lep(LepP, LepM)
+    print("lep_kin shape:", lep_kin.shape)
+    lep_kin.head(5)
+
+    MET_kin = processor.process_MET(MET)
+    print("MET_kin shape:", MET_kin.shape)
+    MET_kin.head(5)
+
+    dinu_kin = processor.process_dinu(NuP, NuM)
+    print("dinu_kin shape:", dinu_kin.shape)
+    dinu_kin.head(5)
+
+    CGLMP_kin = processor.process_CGLMP(CGLMP)
+    print("CGLMP shape:", CGLMP_kin.shape)
+    CGLMP_kin.head(5)
+
+    del processor  # Clear the instance
+    gc.collect()
